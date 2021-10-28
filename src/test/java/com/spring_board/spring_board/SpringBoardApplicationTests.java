13ddbf5484d15e49e584d264eed4278cc3d70d7e
kms1572 <alstn75269@gmail.com>
2021-10-28 14:04:30 +0900

    Initial commit

---
+++ src/test/java/com/spring_board/spring_board/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_board.spring_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
